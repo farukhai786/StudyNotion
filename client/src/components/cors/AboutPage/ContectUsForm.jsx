ba1d95fc43c330e4a82947d1ef0e49336b946890
Farukh Khan <farukhali4343@gmail.com>
2025-07-23 08:32:20 +0530

    fix: countryCode.js import for Vercel

--- client/src/components/cors/AboutPage/ContectUsForm.jsx
+++ client/src/components/cors/AboutPage/ContectUsForm.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useForm } from "react-hook-form"
-import countryCodes from "../../../data/countryCode"
+import countryCodes from "../../../data/countryCode.js"
 const ContactForm = () => {
   const {
     register,
