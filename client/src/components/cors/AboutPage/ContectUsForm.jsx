c3e07ec75ef8dd885576ec3acf301ca1cb2ecbd2
farukhai786 <145565739+farukhai786@users.noreply.github.com>
2025-07-27 16:26:05 +0530

    Update ContectUsForm.jsx

--- client/src/components/cors/AboutPage/ContectUsForm.jsx
+++ client/src/components/cors/AboutPage/ContectUsForm.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useForm } from "react-hook-form"
-import {countryCodes} from "../../../data/CountryCode"
+import countryCodes from "../../../data/CountryCode"
 const ContactForm = () => {
   const {
     register,
