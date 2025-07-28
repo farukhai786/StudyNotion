0159e745c7b1ae4075dcd43f894a6413b08c3f91
farukhai786 <145565739+farukhai786@users.noreply.github.com>
2025-07-28 08:33:11 +0530

    Update database.js

--- backend/Config/database.js
+++ backend/Config/database.js
@@ -16,12 +16,19 @@
 
 
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-exports.connect = () =>
-  mongoose.connect("mongodb://localhost:27017/StudyNotion")  // यहाँ direct MongoDB URI लिखा है
+
+dotenv.config();
+
+exports.connect = () => {
+  mongoose
+    .connect(process.env.MONGO_URL)  
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => {
       console.error('DB error:', err.message);
       process.exit(1);
     });
+};
+
