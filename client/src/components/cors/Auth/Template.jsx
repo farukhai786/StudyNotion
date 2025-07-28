269980579b3b9065062a4f66c0e45dda3cea1b69
farukhai786 <145565739+farukhai786@users.noreply.github.com>
2025-07-28 13:37:14 +0530

    Update Template.jsx

--- client/src/components/cors/Auth/Template.jsx
+++ client/src/components/cors/Auth/Template.jsx
@@ -4,7 +4,7 @@ import SignupForm from "../Auth/SignupForm.jsx";
 import LoginForm from "../Auth/LoginForm.jsx";
 import { FcGoogle } from "react-icons/fc";
 
-const Template = ({ title, desc1, desc2, image, formType, }) => {
+const Template = ({ title, desc1, desc2, image, formType }) => {
   return (
     <div className="flex w-9/12 flex-col-reverse lg:flex-row py-12 mx-auto gap-y-0 gap-x-12 justify-between">
       <div className="w-11/12 max-w-[450px] mx-0 text-white">
