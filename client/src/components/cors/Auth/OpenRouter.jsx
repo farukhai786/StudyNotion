1508fd5b2e8f2d8f2a5ccde668e5b27405ac0b56
farukhai786 <145565739+farukhai786@users.noreply.github.com>
2025-08-03 16:34:00 +0530

    Update OpenRouter.jsx

--- client/src/components/cors/Auth/OpenRouter.jsx
+++ client/src/components/cors/Auth/OpenRouter.jsx
@@ -1,4 +1,4 @@
-// Only allow access if token does NOT exist (i.e. user is NOT logged in)
+
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
@@ -6,8 +6,7 @@ import { Navigate } from 'react-router-dom'
 const OpenRoute = ({ children }) => {
   const { token } = useSelector((state) => state.auth)
 
-  // ❌ If logged in → redirect to dashboard
-  // ✅ If not logged in → allow
+
   return !token ? children : <Navigate to="/dashboard/my-profile" replace />
 }
 
