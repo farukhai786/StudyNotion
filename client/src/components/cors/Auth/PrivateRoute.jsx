68e5115d04d1a9e8bac4bab895e1f22da53a8efd
farukhai786 <145565739+farukhai786@users.noreply.github.com>
2025-08-03 16:34:30 +0530

    Update PrivateRoute.jsx

--- client/src/components/cors/Auth/PrivateRoute.jsx
+++ client/src/components/cors/Auth/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-// Only allow access if token exists (i.e. user is logged in)
+
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
@@ -6,8 +6,6 @@ import { Navigate } from 'react-router-dom'
 const PrivateRoute = ({ children }) => {
   const { token } = useSelector((state) => state.auth)
 
-  // ✅ If logged in → allow
-  // ❌ If not logged in → redirect to /login
   return token ? children : <Navigate to="/login" replace />
 }
 
