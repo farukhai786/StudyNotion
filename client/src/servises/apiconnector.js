e675ca5ffa36bde679f6679cead4fc1d7d89bfe7
farukhai786 <145565739+farukhai786@users.noreply.github.com>
2025-08-03 17:03:33 +0530

    Update apiconnector.js

--- client/src/servises/apiconnector.js
+++ client/src/servises/apiconnector.js
@@ -4,11 +4,11 @@ export const axiosInstance = axios.create({});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
   return axiosInstance({
-    method: `${method}`,   // e.g., "POST", "GET", etc.
-    url: `${url}`,         // API endpoint
-    data: bodyData ? bodyData : null,  // for POST, PUT, etc.
-    headers: headers ? headers : null, // optional headers
-    params: params ? params : null     // for query params (GET)
+    method: `${method}`,   
+    url: `${url}`,       
+    data: bodyData ? bodyData : null, 
+    headers: headers ? headers : null,
+    params: params ? params : null   
   });
 }
 
